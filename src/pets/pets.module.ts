586c75efced0e122a5857536116a7a5beb54b9c1
saeedmashmoli <saeedmashmoli1415@gmail.com>
2022-01-12 13:53:02 +0330

    refactor

--- src/pets/pets.module.ts
+++ src/pets/pets.module.ts
@@ -3,11 +3,10 @@ import { PetsService } from './pets.service';
 import { PetsResolver } from './pets.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pet } from './entities/pet.entity';
-import { OwnersService } from '../owners/owners.service';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([Pet])],
   providers: [PetsService,PetsResolver],
-  // exports: []
 })
 export class PetsModule {}
--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -3,14 +3,13 @@ import { PetsService } from './pets.service';
 import { Pet } from './entities/pet.entity';
 import { CreatePetInput } from './dto/create-pet.input';
 import { UpdatePetInput } from './dto/update-pet.input';
-import { OwnersService } from 'src/owners/owners.service';
 import { Owner } from 'src/owners/entities/owner.entity';
 
 @Resolver(() => Pet)
 export class PetsResolver {
   constructor(private petsService: PetsService,) {}
   
-  @ResolveField(returns => Owner)
+  @ResolveField(() => Owner)
   owner(@Parent() pet: Pet): Promise<Owner> {
     return this.petsService.getOwner(pet.id);
   }
